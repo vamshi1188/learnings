4bcc6d1b6876412c36b880c25947ea7f3cca7d6a
vamshi <vamshigodev@gmail.com>
2025-07-06 21:27:17 +0530

    s
    Signed-off-by: vamshi <vamshigodev@gmail.com>

--- Frontendmasters/goforprodev/femproject/main.go
+++ Frontendmasters/goforprodev/femproject/main.go
@@ -1,7 +1,7 @@
 package main
 
 import (
-	"github.com/vamshi1188/Femproject/internal/app"
+	"github.com/vamshi1188/learnings/Frontendmasters/goforprodev/femproject/internal/app"
 )
 
 func main() {
