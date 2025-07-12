b2e0f93cba8d440ea59db19da9b28e222d8e5d79
vamshi1188 <saivamshik11@gmail.com>
2025-07-13 00:09:12 +0530

    added get workouts by id function
    Signed-off-by: vamshi1188 <saivamshik11@gmail.com>

--- Frontendmasters/goforprodev/femproject/internal/stores/workoput_store.go
+++ Frontendmasters/goforprodev/femproject/internal/stores/workoput_store.go
@@ -81,6 +81,56 @@ func (pg *PostgresWorkoutStore) CreateWorkout(workout *Workout) (*Workout, error
 
 func (pg *PostgresWorkoutStore) GetWorkoutByID(id int64) (*Workout, error) {
 	workout := &Workout{}
+	query := `
+	
+	SELECT Id,title,description,duration_minutes,calories_burned
+	FROM workouts
+	WHERE id= $1
+	`
+	err := pg.db.QueryRow(query, id).Scan(&workout.ID, &workout.Title, &workout.Description, &workout.DurationMinutes, &workout.CaloriesBurned)
+
+	if err == sql.ErrNoRows {
+		return nil, nil
+	}
+
+	if err != nil {
+		return nil, err
+	}
+
+	// lets get entries
+
+	entryQuery := `
+	SELECT id,excercise_name,sets,reps,duration_seconds,weight,notes,order_index
+	FROM workout_id = $1
+	ORDER BY order_index
+	`
+
+	rows, err := pg.db.Query(entryQuery, id)
+
+	if err != nil {
+		return nil, err
+	}
+
+	defer rows.Close()
+
+	for rows.Next() {
+		var entry WorkoutEntry
+		err := rows.Scan(
+			&entry.ID,
+			&entry.ExerciseName,
+			&entry.Sets,
+			&entry.Reps,
+			&entry.DurationSeconds,
+			&entry.Weight,
+			&entry.Notes,
+			&entry.OrderIndex,
+		)
+		if err != nil {
+			return nil, err
+		}
+
+		workout.Entries = append(workout.Entries, entry)
+	}
 
 	return workout, nil
 }
