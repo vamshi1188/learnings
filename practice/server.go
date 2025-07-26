919733f06a77e9d48e1e3894cb450257d63ab458
vamshi <vamshigodev@gmail.com>
2025-07-26 22:51:45 +0530

    g
    Signed-off-by: vamshi <vamshigodev@gmail.com>

--- practice/server.go
+++ practice/server.go
@@ -2,21 +2,51 @@ package main
 
 import (
 	"fmt"
+	"html/template"
 	"net/http"
+
+	"github.com/gin-gonic/gin"
 )
 
+// Global template variable - parse once at startup
+var htmlTemplate *template.Template
+
+// LoadTemplate renders HTML files
+func LoadTemplate(c *gin.Context) {
+	// Execute template directly to the response writer
+	err := htmlTemplate.Execute(c.Writer, nil)
+	if err != nil {
+		c.String(http.StatusInternalServerError, "Internal Server Error while executing template")
+		return
+	}
+
+	// Set content type for HTML
+	c.Header("Content-Type", "text/html; charset=utf-8")
+}
+
 func main() {
-	fmt.Println("Welcme to my server..")
-	http.HandleFunc("/count", Count)
-	http.HandleFunc("/", Home)
-	http.ListenAndServe("localhost:8080", nil)
+	// Parse template once at startup
+	var err error
+	htmlTemplate, err = template.ParseFiles("web.html")
+	if err != nil {
+		fmt.Printf("Error parsing template: %v\n", err)
+		return
+	}
 
-}
+	// Create Gin router
+	app := gin.Default()
+	fmt.Println("Welcome to my Gin server..")
 
-func Home(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintln(w, "go to http://localhost:8080/count")
-}
+	app.GET("/", func(c *gin.Context) {
+		c.String(http.StatusOK, "Go to http://localhost:8080/count")
+	})
 
-func Count(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintln(w, "you are on count page")
+	app.GET("/count", LoadTemplate)
+
+	app.GET("/clickme", func(c *gin.Context) {
+		c.String(http.StatusOK, "you have clicked")
+	})
+
+	fmt.Println("Server starting on http://localhost:8080")
+	app.Run(":8080")
 }
