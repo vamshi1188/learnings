b353661931aa6072a731a62d039fe72c7af87348
vamshi <vamshigodev@gmail.com>
2025-12-01 21:09:05 +0530

    completed sql tutorial
    Signed-off-by: vamshi <vamshigodev@gmail.com>

--- youtube/codingwithpatrik/sql/database/order.go
+++ youtube/codingwithpatrik/sql/database/order.go
@@ -20,18 +20,51 @@ func (r *OrderRepository) CreateTable() error {
 	)`)
 	return err
 }
-func (r *OrderRepository) InsertData() error {
+func (r *OrderRepository) InsertData(order Order) error {
 
-	id := 3
-	product := ""
-	ammount := 1
-	_, err := r.DB.Exec(`INSERT INTO orders(id,product,amount)VALUES($1,$2,$3)`, id, product, ammount)
+	_, err := r.DB.Exec(`INSERT INTO orders(product,amount)VALUES($1,$2)`, order.Product, order.Amount)
 	return err
 }
-func (r *OrderRepository) DeleteData() error {
-
-	id := 3
+func (r *OrderRepository) DeleteData(id int) error {
 
 	_, err := r.DB.Exec(`DELETE FROM orders WHERE id = $1;`, id)
 	return err
+}
+func (r *OrderRepository) GetAll() ([]Order, error) {
+
+	rows, err := r.DB.Query("SELECT * FROM orders")
+	if err != nil {
+		return nil, err
+	}
+	defer rows.Close()
+
+	var orders []Order
+
+	for rows.Next() {
+		var order Order
+		err := rows.Scan(&order.Id, &order.Product, &order.Amount)
+		if err != nil {
+			return nil, err
+		}
+
+		orders = append(orders, order)
+	}
+	return orders, nil
+}
+
+func (r *OrderRepository) GetById(id int) (Order, error) {
+	var order Order
+
+	err := r.DB.QueryRow("SELECT * FROM orders WHERE id = $1", id).Scan(&order.Id, &order.Product, &order.Amount)
+	if err != nil {
+		return Order{}, err
+	}
+	return order, nil
+}
+
+func (r *OrderRepository) UpdateRow(order Order) error {
+
+	_, err := r.DB.Exec("UPDATE orders SET product = $1,amount = $2 WHERE id = $3", order.Product, order.Amount, order.Id)
+	return err
+
 }
--- youtube/codingwithpatrik/sql/main.go
+++ youtube/codingwithpatrik/sql/main.go
@@ -1,12 +1,14 @@
 package main
 
 import (
+	"bufio"
 	"database/sql"
-	"log"
-	"psql/database"
-
-	// "github.com/jackc/pgx/v5"
+	"fmt"
 	_ "github.com/lib/pq"
+	"log"
+	"os"
+	"psql/database"
+	"strconv"
 )
 
 func main() {
@@ -20,13 +22,106 @@ func main() {
 
 	defer dbConnection.Close()
 	OrderRepository := &database.OrderRepository{DB: dbConnection}
+
 	err = OrderRepository.CreateTable()
 	if err != nil {
 		log.Fatal("error in connecting database", err)
 	}
-	err = OrderRepository.DeleteData()
-	if err != nil {
-		log.Fatal("error in inserting data in table ", err)
+	for {
+		PrintText1()
+		// scanner
+		sc := bufio.NewScanner(os.Stdin)
+		choice := ""
+		sc.Scan()
+		choice = sc.Text()
+		choiceint, err := strconv.Atoi(choice)
+
+		if err != nil {
+			log.Fatal("error in string conversion ", err)
+		}
+
+		switch choiceint {
+		case 1:
+
+			fmt.Println("enter product: ")
+			sc.Scan()
+			prdct := sc.Text()
+			fmt.Printf("enter amount of %v\n", prdct)
+			sc.Scan()
+			amount := sc.Text()
+			amountint, err := strconv.Atoi(amount)
+			if err != nil {
+				log.Fatal(err)
+			}
+
+			err = OrderRepository.InsertData(database.Order{Product: prdct, Amount: amountint})
+			if err != nil {
+				log.Fatal("error in inserting data in table ", err)
+			}
+
+		case 2:
+			fmt.Println("enter id:")
+			sc.Scan()
+			id := sc.Text()
+			idint, err := strconv.Atoi(id)
+			if err != nil {
+				log.Fatal(err)
+			}
+			fmt.Println("enter product: ")
+			sc.Scan()
+			prdct := sc.Text()
+			fmt.Printf("enter amount of %v\n", prdct)
+			sc.Scan()
+			amount := sc.Text()
+			amountint, err := strconv.Atoi(amount)
+			if err != nil {
+				log.Fatal(err)
+			}
+			err = OrderRepository.UpdateRow(database.Order{Id: idint, Product: prdct, Amount: amountint})
+
+		case 3:
+
+			fmt.Println("enter id You want to delete: ")
+			sc.Scan()
+			id := sc.Text()
+			idint, err := strconv.Atoi(id)
+			if err != nil {
+				log.Fatal(err)
+			}
+			err = OrderRepository.DeleteData(idint)
+
+		case 4:
+			fmt.Println("enter id: ")
+			sc.Scan()
+			id := sc.Text()
+			idint, err := strconv.Atoi(id)
+			if err != nil {
+				log.Fatal(err)
+			}
+			order, err := OrderRepository.GetById(idint)
+			if err != nil {
+				log.Fatal("error in getting order by id ", err)
+			}
+			fmt.Println(order)
+		case 5:
+			rows, err := OrderRepository.GetAll()
+			if err != nil {
+				log.Fatal(err)
+			}
+			fmt.Println(rows)
+
+		}
 	}
 
+}
+
+func PrintText1() {
+	fmt.Println()
+	fmt.Println("Welcome to myecomerce!")
+	fmt.Println("choose one option below : ")
+	fmt.Println("1 - for adding product in database")
+	fmt.Println("2 - for update product in database")
+	fmt.Println("3 - delete product in database ")
+	fmt.Println("4 - get data by id")
+	fmt.Println("5 - get all data")
 }
